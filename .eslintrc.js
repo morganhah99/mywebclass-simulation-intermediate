3c061cd9272f26ec6f330dda990b658acc39320e
aron-s <aronshrestha331@gmail.com>
2023-03-27 18:27:22 -0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,13 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
+    "extends": "eslint:recommended",
     "overrides": [
     ],
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
-    "plugins": [
-        "react"
-    ],
     "rules": {
     }
 }
